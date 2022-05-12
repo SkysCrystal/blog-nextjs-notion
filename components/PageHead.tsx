4e8ab076ff8680fef254cf5665b9da18865351ec
Simon Shi <shizijun2006@126.com>
2022-05-12 14:30:08 +0800

    Merge branch 'zhimiaoli-main'

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -79,6 +79,25 @@ export const PageHead: React.FC<
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
+      
+      # Add GA
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-G6E86FELGH`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G6E86FELGH', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+
     </Head>
   )
 }
